3d1759af66985502863a316464c7a8d97992d3d0
wooden <wooden@15fb7048-6c44-0410-840a-904aa505184c>
2008-02-10 13:46:57 +0000

    0.3 relase

---
+++ tags/mugendownloader-0.3/mugendownloader.py
@@ -0,0 +1,243 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# main.py
+# Copyright (C) WoodenJesus 2007 <woodenjesus666@gmail.com>
+# 
+# mugendownlader.py is free software.
+# 
+# You may redistribute it and/or modify it under the terms of the
+# GNU General Public License, as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option)
+# any later version.
+# 
+# main.py is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with main.py.  If not, write to:
+# 	The Free Software Foundation, Inc.,
+# 	51 Franklin Street, Fifth Floor
+# 	Boston, MA  02110-1301, USA.
+
+
+__version__ = '0.3'
+
+import inspect
+import ctypes
+
+import pygtk, gtk, gtk.glade, gobject
+import ansidloader
+import threading, time
+import sys, os
+
+### http://sebulba.wikispaces.com/recipe+thread2 killing thread	######
+
+def _async_raise(tid, exctype):
+    """raises the exception, performs cleanup if needed"""
+    if not inspect.isclass(exctype):
+        raise TypeError("Only types can be raised (not instances)")
+    res = ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, ctypes.py_object(exctype))
+    if res == 0:
+        raise ValueError("invalid thread id")
+    elif res != 1:
+        # """if it returns a number greater than one, you're in trouble, 
+        # and you should call it again with exc=NULL to revert the effect"""
+        ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, 0)
+        raise SystemError("PyThreadState_SetAsyncExc failed")
+######################################################################
+
+############### From pyGTK FAQ #######################################
+def do_gui_operation(function, *args, **kw):
+    def idle_func():
+        gtk.gdk.threads_enter()
+        try:
+            function(*args, **kw)
+            return False
+        finally:
+            gtk.gdk.threads_leave()
+    gobject.idle_add(idle_func)
+#####################################################################
+
+
+#run download thread
+
+class RunDload ( threading.Thread ):
+	
+	def __init__ (self, window, args=[]):
+		threading.Thread.__init__(self)
+		self.args = args
+		self.work = threading.Event()
+		self.window = window
+		#print self.args
+
+	def run (self):
+		self.work.set()
+		self.download = ansidloader.AnsiDloader()
+		self.download.findsub(*self.args)
+		do_gui_operation (self.window.Finish)
+		self.work.clear()
+		#self.join()
+	
+	def stop(self):
+		self.work.clear()
+		self.terminate()
+		self.join()
+
+
+### http://sebulba.wikispaces.com/recipe+thread2 killing thread	######
+	def _get_my_tid(self):
+		"""determines this (self's) thread id"""
+		if not self.isAlive():
+			raise threading.ThreadError("the thread is not active")
+        
+        # do we have it cached?
+		if hasattr(self, "_thread_id"):
+			return self._thread_id
+        
+        # no, look for it in the _active dict
+		for tid, tobj in threading._active.items():
+			if tobj is self:
+				self._thread_id = tid
+				return tid
+        
+		raise AssertionError("could not determine the thread's id")
+        
+	def raise_exc(self, exctype):
+		"""raises the given exception type in the context of this thread"""
+		_async_raise(self._get_my_tid(), exctype)
+    
+	def terminate(self):
+		"""raises SystemExit in the context of the given thread, which should 
+		cause the thread to exit silently (unless caught)"""
+		self.raise_exc(SystemExit)
+######################################################################
+
+#initialize main window
+class MugenWindow:
+
+	def __init__(self):
+
+		self.glade = gtk.glade.XML("interface.glade")
+		self.window = self.glade.get_widget('main')
+		
+		self.exit = self.glade.get_widget('close')
+		self.exit.connect("clicked", self.Exit)
+		self.window.connect ("destroy", self.Exit)
+			
+		self.about = self.glade.get_widget('about')
+		self.about.connect("clicked", self.About)
+		
+		self.ok = self.glade.get_widget('ok')
+		self.ok.connect("clicked", self.Download)
+		
+		self.cancel = self.glade.get_widget('cancel')
+		self.cancel.connect("clicked", self.CancelDownload)
+		
+		self.selectOd = self.glade.get_widget('selectOd')
+		self.selectDo = self.glade.get_widget('selectDo')
+		
+		self.autor = self.glade.get_widget('autor')
+		
+		self.activateOd = self.glade.get_widget('activateOd')
+		self.activateDo = self.glade.get_widget('activateDo')
+		
+		self.activateOd.connect("toggled", self.CalendarHandle, self.selectOd)
+		self.activateDo.connect("toggled", self.CalendarHandle, self.selectDo)
+		
+		self.find = self.glade.get_widget('find')
+		self.dirchooser = self.glade.get_widget('dirchooser')
+		self.dirchooser.connect("selection-changed", self.UpdateDefault)
+		self.cancel.connect("clicked", self.CancelDownload)
+		self.pageno = self.glade.get_widget('pageno')
+		self.output = self.glade.get_widget('output')
+		
+		if os.path.isfile ('default.conf'):
+			default = open ('default.conf', 'r')
+			dir = default.read()
+			if os.path.isdir (dir):
+				self.dirchooser.set_filename(dir)
+			default.close()
+		
+		self.text = self.output.get_buffer()
+		self.download = RunDload (self.window, (self.find.get_text(), self.dirchooser.get_filename(), self.OutputHandler))
+		
+		self.window.show()
+
+	def UpdateDefault(self, *trash):
+		if os.path.isfile ('default.conf'):
+			os.remove ('default.conf')
+		default = open ('default.conf', 'w')
+		default.write(self.dirchooser.get_filename())
+		default.close()
+		
+	def Download (self, button):
+		if not self.download.work.isSet():
+			if self.find.get_text() != '':
+				
+				od = (0, 0, 0)
+				do = (0, 0, 0)
+				
+				if self.activateOd.get_active() == True:
+					od = self.selectOd.get_date()
+					#od[1] = od[1] + 1
+					
+				if self.activateDo.get_active() == True:
+					do = self.selectDo.get_date()	
+					#do[1] = do[1] + 1		
+				
+				self.download = RunDload (self, (self.find.get_text(), self.dirchooser.get_filename(),  self.OutputHandler, self.pageno.get_value(), self.autor.get_text(), od, do ))
+				self.ok.hide()
+				self.cancel.show()
+				self.download.start()
+	
+	def Finish (self):
+		self.cancel.hide()
+		self.ok.show()
+	
+	def CancelDownload (self, button):
+		if self.download.work.isSet():
+			self.download.stop()
+			self.cancel.hide()
+			self.ok.show()
+			
+	def CalendarHandle (self, toggle, calendar):
+		if toggle.get_active() == True:
+			calendar.show()
+		else: 
+			calendar.hide()
+		pass
+			
+	def About (self, *arg):
+		dialog = gtk.AboutDialog()                                                                                                                                                                     
+		dialog.set_name("Mugen ∞ Downloader " + __version__)                                                                                                                                                                  
+		dialog.set_copyright("WoodenJesus")
+		dialog.set_comments("Law of Mentat relase")                                                                                                                                    
+		dialog.set_website("http://mugendownloader.berlios.de")                                                                                                                                                   
+        
+        ## Close dialog on user response                                                                                                                                                               
+		dialog.connect ("response", lambda d, r: d.destroy())                                                                                                                                          
+		dialog.show() 
+
+
+
+	def Exit(self, *arg):
+		sys.exit(0)	
+	
+	def LOL	(self, *args):
+		self.aboutwindow.hide()
+		
+	def OutputHandler (self, string):
+		self.output.scroll_to_iter(self.text.get_end_iter(), 0)
+		self.text.insert (self.text.get_end_iter(), string)
+		self.output.scroll_to_iter(self.text.get_end_iter(), 0)
+		
+def main():
+	gtk.gdk.threads_init()
+	gtk.gdk.threads_enter()
+	run = MugenWindow()
+	gtk.main()
+	gtk.gdk.threads_leave()
+
+main()
---
+++ tags/mugendownloader-0.3/win32/mugen2exe.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# main.py
+# Copyright (C) WoodenJesus 2007 <woodenjesus666@gmail.com>
+# 
+# main.py is free software.
+# 
+# You may redistribute it and/or modify it under the terms of the
+# GNU General Public License, as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option)
+# any later version.
+# 
+# main.py is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with main.py.  If not, write to:
+# 	The Free Software Foundation, Inc.,
+# 	51 Franklin Street, Fifth Floor
+# 	Boston, MA  02110-1301, USA.
+
+
+
+from distutils.core import setup
+import py2exe
+import glob
+
+opts = {
+    "py2exe": {
+        "includes": "pango,atk,gobject,cairo, pangocairo, gtk",
+        "dll_excludes": [
+        "iconv.dll","intl.dll","libatk-1.0-0.dll",
+        "libgdk_pixbuf-2.0-0.dll","libgdk-win32-2.0-0.dll",
+        "libpangoft2-1.0-0.dll",
+        "libcairo-2.dll","libpangocairo-1.0-0.dll",
+        "libglib-2.0-0.dll","libgmodule-2.0-0.dll",
+        "libgobject-2.0-0.dll","libgthread-2.0-0.dll",
+        "libgtk-win32-2.0-0.dll","libpango-1.0-0.dll",
+        "libpangowin32-1.0-0.dll"],
+        }
+    }
+
+setup(
+    name = "mugendownloader",
+    description = "Prosty program do pobierania napisow z animesub.info",
+    version = "0.1",
+    windows = [
+        {"script": "mugendownloader.py",
+        "icon_resources": [(1, "mugendownloader.ico")]
+        }
+    ],
+    options=opts,
+    data_files=[("GPL"),
+                ("pixmaps", glob.glob("pixmaps/*.png")),
+                ("interface.glade")
+    ],
+)
